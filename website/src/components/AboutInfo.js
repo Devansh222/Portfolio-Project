0cb639af6ffa3a55b842ca32ad52f9fa89107d6a
Shipra <shipramzn7300@gmail.com>
2023-03-13 19:52:40 +0530

    adding Portfolio code

---
+++ website/src/components/AboutInfo.js
@@ -0,0 +1,17 @@
+import "./AboutInfoStyles.css"
+
+import React from 'react'
+
+const AboutInfo = () => {
+  return <div className="about">
+    <div className="left">
+        <h1> Who Am I </h1>
+        <p> I have good knowledge of Data structures and Algorithms. I have done coding on platforms like leetcode and hacker rank. I have deep knowledge of OOPs and DBMS as well. I have participated in Flipkart Grid 4.0 , 2022
+
+        </p>
+    </div>
+
+  </div>
+}
+
+export default AboutInfo;
